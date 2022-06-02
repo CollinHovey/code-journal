6b7152c86d2a8a3efc3f0fec7e49c7f9b83f70a2
Collin <collinhovey@gmail.com>
2022-06-02 18:21:03 +0000

    Completed Listen for Submit Event and Reset Values

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 var $photoURL = document.querySelector('img');
 var $newPhotoURL = document.querySelector('input#photo-url');
+var $title = document.querySelector('input#title');
+var $notes = document.querySelector('textarea#notes');
+var $submit = document.querySelector('button.submit');
 
 function updatePhoto(event) {
   if (event.target.value === '') {
@@ -10,3 +13,20 @@ function updatePhoto(event) {
 }
 
 $newPhotoURL.addEventListener('input', updatePhoto);
+
+function addEntry(event) {
+  event.preventDefault();
+  var newEntry = {};
+  newEntry.entryId = data.nextEntryId;
+  newEntry.title = $title.value;
+  newEntry.photoURL = $newPhotoURL.value;
+  newEntry.notes = $notes.value;
+  data.entries.push(newEntry);
+  data.nextEntryId += 1;
+  $newPhotoURL.value = '';
+  $photoURL.setAttribute('src', 'images/placeholder-image-square.jpg');
+  $title.value = '';
+  $notes.value = '';
+}
+
+$submit.addEventListener('click', addEntry);
